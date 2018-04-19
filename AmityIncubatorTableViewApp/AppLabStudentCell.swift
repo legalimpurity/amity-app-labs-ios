b131bd9f6579c66c6413d1d1302b83786939bf5a
rajat khanna <legalimpurity@gmail.com>
2018-04-19 14:56:04 +0530

    initial project for table view.

---
+++ AmityIncubatorTableViewApp/AppLabStudentCell.swift
@@ -0,0 +1,17 @@
+//
+//  AppLabStudentCell.swift
+//  AmityIncubatorTableViewApp
+//
+//  Created by Amity AppLabs on 09/04/18.
+//  Copyright © 2018 Amity Innovation Incubator. All rights reserved.
+//
+
+import UIKit
+
+class AppLabStudentCell : UITableViewCell
+{
+    @IBOutlet weak var firstName: UILabel!
+    
+    @IBOutlet weak var lastName: UILabel!
+    
+}
--- AmityIncubatorTableViewApp/ViewController.swift
+++ AmityIncubatorTableViewApp/ViewController.swift
@@ -8,18 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var dataSourceTitles : Array = [String]()
+    var dataSourceSubTitles : Array = [String]()
+
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSourceTitles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AppLabStudentItem") as! AppLabStudentCell
+        cell.firstName.text = dataSourceTitles[indexPath.row]
+        cell.lastName.text = dataSourceSubTitles[indexPath.row]
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+        dataSourceTitles.append("Animesh")
+        dataSourceTitles.append("Sahil")
+        dataSourceTitles.append("Rajat")
 
+        dataSourceSubTitles.append("Verma")
+        dataSourceSubTitles.append("Bhutani")
+        dataSourceSubTitles.append("Khanna")
+
+    }
 
 }
 
