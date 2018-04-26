54acb0585afb866e14059f9dbbf3fd8ab1bfb1a2
rajat khanna <legalimpurity@gmail.com>
2018-04-26 15:42:30 +0530

    added CoreData Code done today.

---
+++ AmityIncubatorTableViewApp/AddAppLabStudent.swift
@@ -0,0 +1,9 @@
+//
+//  AddAppLabStudent.swift
+//  AmityIncubatorTableViewApp
+//
+//  Created by Amity AppLabs on 26/04/18.
+//  Copyright © 2018 Amity Innovation Incubator. All rights reserved.
+//
+
+import Foundation
