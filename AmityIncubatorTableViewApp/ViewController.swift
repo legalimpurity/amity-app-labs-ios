c46dd48d4417b23a06b4fbc4f439a92a3372ec2a
rajat khanna <legalimpurity@gmail.com>
2018-04-26 10:52:51 +0530

    api list fetching added.

--- AmityIncubatorTableViewApp/ViewController.swift
+++ AmityIncubatorTableViewApp/ViewController.swift
@@ -7,8 +7,10 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var appLabStudentsTableView: UITableView!
     
     var dataSourceTitles : Array = [String]()
     var dataSourceSubTitles : Array = [String]()
@@ -28,16 +30,45 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     override func viewDidLoad() {
         super.viewDidLoad()
+//        getMockDataFunction()
+        getActualDataFunction()
+    }
 
+    func getMockDataFunction() {
         dataSourceTitles.append("Animesh")
         dataSourceTitles.append("Sahil")
         dataSourceTitles.append("Rajat")
-
+        
         dataSourceSubTitles.append("Verma")
         dataSourceSubTitles.append("Bhutani")
         dataSourceSubTitles.append("Khanna")
-
     }
+    
+    func getActualDataFunction()
+    {
+        Alamofire.request("http://194.168.1.208:8888/MyFirstAPI.php")
+            .responseJSON { response in
+                switch response.result
+                {
+                case .failure(_) :
+                    return
+                case .success(let data):
+                    guard let json = data as? [String:AnyObject] else { return }
+                    guard var maybeStudentResponseVariable = json["studentapiresponse"] as? [AnyObject] else { return }
+                    for app_lab_student in maybeStudentResponseVariable
+                    {
+                        guard var studentFirstName = app_lab_student["first_name"] as? String else { return }
+                        self.dataSourceTitles.append(studentFirstName)
 
+                        guard var studentLastName = app_lab_student["last_name"] as? String else { return }
+
+                        guard var studentAge = app_lab_student["age"] as? String else { return }
+
+                        self.dataSourceSubTitles.append(studentAge)
+                    }
+                    self.appLabStudentsTableView.reloadData()
+                }
+        }
+    }
 }
 
